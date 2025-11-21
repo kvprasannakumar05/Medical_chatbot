709b1f4ad382e1c3a6cdcf7028d7dc8c43fc503f
K V Prasanna kumar <kvprasannakumar005@gmail.com>
2025-11-21 15:58:00 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+mkdir -p src
+mkdir -p research 
+
+touch src/__init__.py
+touch src/helpers.py
+touch src/prompts.py
+touch research/trials.py
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+
+
+echo "files created successfully.."
+
